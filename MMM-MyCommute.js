81ae24e833fb790d668ad1799772990564b1139c
Jeff Clarke <jeff.clarke@kineticsocial.com>
2017-05-12 11:56:42 -0400

    Initial commit with changes to the fork

---
+++ MMM-MyCommute.js
@@ -0,0 +1,182 @@
+
+/*********************************
+
+  Magic Mirror Module: 
+  MMM-MyCommute
+  By Jeff Clarke
+
+  Fork of mrx-work-traffic
+  By Dominic Marx
+  https://github.com/domsen123/mrx-work-traffic
+
+  MIT Licensed
+ 
+*********************************/
+
+Module.register('MMM-MyCommute', {
+
+  defaults: {
+    apikey: '',
+    showHeader: true,
+    headerText: 'My Commute',
+    origin: '65 Front St W, Toronto, ON M5J 1E6',
+    startTime: '00:00',
+    endTime: '23:59',
+    destinations: [
+      {
+        destination: '40 Bay St, Toronto, ON M5J 2X2',
+        label: 'Air Canada Centre',
+        mode: 'walking',
+        time: null
+      },
+      {
+        destination: '317 Dundas St W, Toronto, ON M5T 1G4',
+        label: 'Art Gallery of Ontario',
+        mode: 'transit',
+        time: null
+      },
+      {
+        destination: '55 Mill St, Toronto, ON M5A 3C4',
+        label: 'Distillery',
+        mode: 'bicycling',
+        time: null
+      },
+      {
+        destination: '6301 Silver Dart Dr, Mississauga, ON L5P 1B2',
+        label: 'Pearson Airport',
+        time: null
+      }
+    ]
+  },
+
+  // Define required scripts.
+  getScripts: function() {
+    return ["moment.js"];
+  },
+  
+  // Define required styles.
+  getStyles: function () {
+    return ["MMM-MyCommute.css", "font-awesome.css"];
+  },
+
+  // Icons to use for each transportation mode
+  symbols: {
+    'driving':    'car',
+    'walking':    'walk',
+    'bicycling':  'bike',
+    'transit':    'streetcar'
+  },
+  
+
+  start: function() {
+
+    Log.info('Starting module: ' + this.name);
+
+    this.loaded = false;
+    this.payload = [];
+    for(var i = 0; i<this.config.destinations.length; i++) {
+      var _url = 'https://maps.googleapis.com/maps/api/directions/json' + this.getParams(this.config.destinations[i]);
+      this.payload.push( {url: _url, label: this.config.destinations[i].label} );
+    }
+
+    this.askGoogle();
+    var self = this;
+
+    setInterval(function(){
+      self.askGoogle();
+    },600000);
+      
+  },
+  
+  getParams: function(dest) {
+    var params = '?';
+    params += 'origin=' + encodeURIComponent(this.config.origin);
+    params += '&destination=' + encodeURIComponent(dest.destination);
+    params += '&key=' + this.config.apikey;
+    params += '&mode=' + (dest.mode == null || '' ? 'driving' : dest.mode);
+    return params;
+  },
+  
+  askGoogle: function() {
+    console.log("getting Google Traffic");
+    this.sendSocketNotification("GOOGLE_TRAFFIC_GET", this.payload);
+  },
+  
+  // Override dom generator.
+  getDom: function() {
+
+    var now = moment();
+    var startTimeSplit = this.config.startTime.split(":");
+    var endTimeSplit = this.config.endTime.split(":");
+    var startTime = moment().hour(startTimeSplit[0]).minute(startTimeSplit[1]);
+    var endTime = moment().hour(endTimeSplit[0]).minute(endTimeSplit[1]);
+
+
+    var wrapper = document.createElement("div");
+    
+    /*
+      Only show the module between the config's startTime and endTime parameters
+    */
+    if( now.isSameOrAfter(startTime) && now.isSameOrBefore(endTime) ){
+
+      if (this.config.showHeader) {      
+        var header = document.createElement("header");
+        header.classList.add("module-header");
+        header.innerHTML = this.config.headerText;
+        wrapper.appendChild(header);
+      } 
+
+      var table = document.createElement("table");
+      table.className = "small";
+      
+      for (var i = 0; i < this.config.destinations.length; i++) {
+        var row = document.createElement("tr");
+        table.appendChild(row);
+        
+        var iconCell = document.createElement("td");
+        iconCell.className = "transit-mode bright";
+        var symbolIcon = 'car';
+        if (this.config.destinations[i].mode != null && this.symbols[this.config.destinations[i].mode] != null) {
+          symbolIcon = this.symbols[this.config.destinations[i].mode];
+        }
+        if (this.config.destinations[i].color) {
+          iconCell.setAttribute("style", "color:" + this.config.destinations[i].color);
+        }
+
+        var svg = document.createElementNS('http://www.w3.org/2000/svg','svg');
+        svg.setAttributeNS(null, "class", "transit-mode-icon");
+        var use = document.createElementNS('http://www.w3.org/2000/svg', "use");
+        use.setAttributeNS("http://www.w3.org/1999/xlink", "href", "modules/MMM-MyCommute/icon_sprite.svg#" + symbolIcon);
+        svg.appendChild(use);
+        iconCell.appendChild(svg);
+
+        row.appendChild(iconCell);
+        
+        var locationCell = document.createElement("td");
+        locationCell.className = "destination-label bright";
+        locationCell.innerHTML = this.config.destinations[i].label;
+        row.appendChild(locationCell);
+        
+        var timeCell = document.createElement("td");
+        timeCell.className = "travel-time";
+        timeCell.innerHTML =  this.config.destinations[i].time ?  this.config.destinations[i].time + ' min' : "<i class='fa fa-spin fa-circle-o-notch'></i>";
+        row.appendChild(timeCell);
+      }
+
+      wrapper.appendChild(table);
+
+    }
+
+    return wrapper;
+  },
+  
+  socketNotificationReceived: function(notification, payload) {
+    if ( notification === 'GOOGLE_TRAFFIC_LIST' ) {
+      for(var i = 0; i<this.config.destinations.length; i++) {
+        if( this.config.destinations[i].label === decodeURIComponent(payload.label))
+          this.config.destinations[i].time = Math.floor( payload.duration / 60 );
+      }
+      this.updateDom();
+    }
+  }
+});
--- README.md
+++ README.md
@@ -1,40 +1,79 @@
-# mrx-work-traffic
-This a module for the [MagicMirror](https://github.com/MichMich/MagicMirror/tree/develop). It shows the time you need to get to work (requires API_KEY). The informations fetched by google.
+# MMM-MyCommute
+This a module for the [MagicMirror](https://github.com/MichMich/MagicMirror/tree/develop).
+
+It shows your commute time using Google's Traffic API (requires API_KEY).
+
+It is a fork of mrx-work-traffic by Dominic Marx.
+https://github.com/domsen123/mrx-work-traffic
 
 # Installation
-1. Navigate into your MagicMirror's `modules`folder and execute `git clone https://github.com/domsen123/mrx-work-traffic`.
+1. Navigate into your MagicMirror's `modules`folder and execute `https://github.com/jclarke0000/MMM-MyCommute.git`.
+2. Go to https://developers.google.com/maps/documentation/javascript/get-api-key and get an API key.
 
 # Config
 |Option|Description|
 |---|---|
-|`apiKey`|API Key from google<br><br>**Type:** `string`<br>This value is **REQUIRED**|
+|`apiKey`|**REQUIRED** API Key from Google<br><br>**Type:** `string`|
+|`showHeader`|Whether to show the module header<br><br>**Type:** `boolean`<br>Defaults to `true`|
+|`headerText`|Text to show in the header<br><br>**Type:** `string`<br>Defaults to `My Commute`|
+|`origin`|**Required** The starting point for your commute.  Usually this is you home address.<br><br>**Type:** `string`<br>This is as you would see it Google Maps.  Example:  `65 Front St W, Toronto, ON M5J 1E6`|
+|`startTime`|The start time of the window during which this module wil be visible.<br><br>**Type:** `string`<br>Must be in 24-hour time format.  Defaults to `00:00` (i.e.: midnight)|
+|`endTime`|The end time of the window during which this module wil be visible.<br><br>**Type:** `string`<br>Must be in 24-hour time format.  Defaults to `23:59` (i.e.: one minute before midnight)|
+|`destinations`|An array of destinations to which you would like to see commute times,<br><br>**Type:** `array`<br>See below for destination options.|
+
+Each object in the `destinations` array has the following parameters:
+
+|Option|Description|
+|---|---|
+|`destination`|**REQUIRED** The address of the destination<br><br>**Type:** `string`|
+|`label`|**REQUIRED** How you would like this displayed on your MagicMirror.<br><br>**Type:** `string`|
+|`mode`|Transportation mode, one of the following: `driving`, `walking`, `bicycling`, `transit`.<br><br>**Type:** `string`<br>Defaults to `driving`.|
+|`color`|If specified, the colour for the icon in hexadecimal format (e.g.: `"#82BAE5"`)<br><br>**Type:** `string`<br>Defaults to white.|
+
+
 
 Here is an example of an entry in `config.js`
 ```
 {
-	module: 'mrx-work-traffic',
-	position: 'top_center',
-	header: 'Arbeitsweg',
-	config: {
-		apikey: 'API_KEY_FROM_GOOGLE',
-		origin: 'Platz der Republik 1, 11011 Berlin',
-		startTime: '00:00',
-		endTime: '23:59',
-		destinations: [
-			{
-				destination: 'Platzl 9, 80331 München',
-				label: 'Hofbräuhaus'
-			}
-		]
-	}
-},
+  module: 'MMM-MyCommute',
+  position: 'top_left',
+  config: {
+    showHeader: true,
+    headerText: 'Traffic',
+    apikey: 'API_KEY_FROM_GOOGLE',
+    origin: '65 Front St W, Toronto, ON M5J 1E6',
+    startTime: '00:00',
+    endTime: '23:59',
+    destinations: [
+      {
+        destination: '14 Duncan St Toronto, ON M5H 3G8',
+        label: 'Air Canada Centre',
+        mode: 'walking',
+        color: '#82E5AA'
+      },
+      {
+        destination: '317 Dundas St W, Toronto, ON M5T 1G4',
+        label: 'Art Gallery of Ontario',
+        mode: 'transit'
+      },
+      {
+        destination: '55 Mill St, Toronto, ON M5A 3C4',
+        label: 'Distillery District',
+        mode: 'bicycling'
+      },
+      {
+        destination: '6301 Silver Dart Dr, Mississauga, ON L5P 1B2',
+        label: 'Pearson Airport'
+      }
+    ]
+  }
+}
 ```
 
-## Dependencies
-- [request](https://www.npmjs.com/package/request) (installed via `npm install`)
 
-## Important Notes
-- This is one of my first projects using Node, so feel free to submit pull requests or post on the issues/wiki and I will do my best to improve the project.
+## Dependencies
+- [request](https://www.npmjs.com/package/request) (Likely already installed, installed via `npm install request`)
 
 ## Special Thanks
 - [Michael Teeuw](https://github.com/MichMich) for creating the awesome [MagicMirror2](https://github.com/MichMich/MagicMirror/tree/develop) project that made this module possible.
+- [Dominic Marx](https://github.com/domsen123) for creating the original mrx-work-traffic that this module meavily borrows upon.
